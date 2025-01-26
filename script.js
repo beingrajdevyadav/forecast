27e34ac51fa1c9e452be2119957b74e355fef521
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-01-27 05:05:17 +0530

    JavaScript API Fetched Successfully.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-console.log("Jai Shree Ram");
-console.log("Jai Shree Krishna");
-console.log("Jai Shree Ganesh");
-console.log("Jai Shree Hanuman");
-console.log("Jai Shree Shyam");
+
+console.log("Jai Shree Shyam Baba ❤🙏");
+
+
+
+async function getWeather(){
+    const cityName = document.getElementById("cityName").value;
+
+    const API_KEY = "6f1ad9775d3cdc8ad9ea341dbf97abe7";
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+
+    try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        console.log(data.cod);
+    } catch (error) {
+        console.error(error);
+    }
+
+}
