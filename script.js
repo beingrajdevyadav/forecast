b97de43cf5ba4e880eece2967b1a30abb4d24005
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-02-01 19:58:06 +0530

    Dynamic Chart Change Features Added.

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
 
 console.log("Jai Shree Shyam Baba ❤🙏");
 
+// =========================================
+let currentChart;
+// =========================================
 
 
-async function getWeather(){
-    const cityName = document.getElementById("cityName").value;
+// ---------------------------------------- 
+//            Get Weather Function
+// ---------------------------------------- 
 
-    const API_KEY = "6f1ad9775d3cdc8ad9ea341dbf97abe7";
-    const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+async function getWeather() {
+  const cityName = document.getElementById("cityName").value;
 
-    try {
-        const response = await fetch(API_URL);
-        const data = await response.json();
-        // console.log(data.cod);
+  const API_KEY = "6f1ad9775d3cdc8ad9ea341dbf97abe7";
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
 
-        if(data.cod == "404"){
-            document.getElementById("cityName").value = ""; 
-            document.getElementById("weatherInfo").innerHTML = `<h2>City Not Found</h2>`;
-            return;
-        }
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    // console.log(data.cod);
 
-        const weatherInfo = `
+    if (data.cod == "404") {
+      document.getElementById("cityName").value = "";
+      document.getElementById("weatherInfo").innerHTML = `<h2>City Not Found</h2>`;
+      return;
+    }
+
+    const weatherInfo = `
             <h2>Weather in ${data.name}</h2>
             <h3>Temperature: ${data.main.temp}°C</h3>
             <h3>Humidity: ${data.main.humidity}%</h3>
             <h3>Weather: ${data.weather[0].main}</h3>
         `;
 
-        document.getElementById("weatherInfo").innerHTML = weatherInfo;
-        console.log(data);
-    } catch (error) {
-        console.error(error);
-    }
+    document.getElementById("weatherInfo").innerHTML = weatherInfo;
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
 
 }
 
@@ -40,11 +47,10 @@ async function getWeather(){
 //            Create Chart Function
 // ---------------------------------------- 
 
-function createChart(){
-    const ctx = document.getElementById('tempChart');
-
-  new Chart(ctx, {
-    type: 'bar',
+function createChart(type) {
+  const ctx = document.getElementById('tempChart');
+ currentChart = new Chart(ctx, {
+    type: type,
     data: {
       labels: ['Feel Like', 'Min Temp', 'Temp', 'Max Temp'],
       datasets: [{
@@ -62,12 +68,30 @@ function createChart(){
           beginAtZero: true
         }
       },
-      maintainHeighAspectRatio : false,
+      maintainHeighAspectRatio: false,
     }
   });
 
-  
+
 
 }
 
-createChart();
+createChart("bar");
+
+
+// ---------------------------------------- 
+//           Active Chart Function
+// ---------------------------------------- 
+const chartTypeBtns = document.querySelectorAll(".chart-type-btn");
+chartTypeBtns.forEach(btn => {
+  btn.addEventListener("click", function () {
+    chartTypeBtns.forEach(btn => btn.classList.remove("active-chart"));
+    btn.classList.add("active-chart");
+
+    let chartType = btn.getAttribute("chartType");
+    // console.log(chartType);
+    currentChart.destroy();
+    createChart(chartType);
+  })
+});
+
